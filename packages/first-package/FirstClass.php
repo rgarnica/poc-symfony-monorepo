e11f25765650445705968dfbd7b63edce953eef2
Raphael Garnica <raphael@upoutsourcing.com>
2022-01-07 12:27:15 -0300

    first commit

---
+++ packages/first-package/FirstClass.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace YourMonorepo\FirstPackage;
+
+final class FirstClass
+{
+
+    public function execute(): string
+    {
+        return 'first class';
+    }
+
+}
